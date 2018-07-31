efb778939a794595c93ec2d154ef2375d102f761
akhilsankar1993 <akhil.sankar@wink.com>
2018-07-31 13:14:16 -0400

    sense and move method

--- robot_localization/main.py
+++ robot_localization/main.py
@@ -4,8 +4,8 @@ from robot import Robot
 # Now we setup our specific example environment.
 
 # intialize grid with 5 cells
-grid = Grid(5)
-grid.set_colors(["green", "red", "red", "green", "green"])
+# grid = Grid(5)
+# grid.set_colors(["green", "red", "red", "green", "green"])
 
 # get PRIOR state of grid
 # print grid.get_state("probability") 
@@ -26,10 +26,25 @@ robot = Robot(
 # With this in mind, the default distribution of the grid is now multiplied by these constants 
 # to determine the POSTERIOR probability distribution of the grid.
     
-print robot.sense(grid, ["red", "green"])
+# print robot.sense(grid, ["red", "green"])
 
 # Now we can test our robot's move method, by creating a new grid and passing it in to the method
 
-move_grid = Grid(5)
-move_grid.set_state("probability", [0, 0.5, 0, 0.5, 0])
-print robot.move(move_grid, 2)
+# move_grid = Grid(5)
+# move_grid.set_state("probability", [1, 0, 0, 0, 0])
+# 
+# for i in range(1000):
+#     robot.move(move_grid, 1)
+# 
+# print move_grid.get_state("probability")
+
+# Now we can test the sense_and_move approach, where we do multiple sense and movement cycles and then perform a posterior 
+# distribution calculation
+
+sense_and_move_grid = Grid(5)
+sense_and_move_grid.set_state("probability", [0.2, 0.2, 0.2, 0.2, 0.2])
+sense_and_move_grid.set_colors(["green", "red", "red", "green", "green"])
+
+measurements = ["red", "green"]
+movements = [1,1]
+print robot.sense_and_move(sense_and_move_grid, measurements, movements)
--- robot_localization/robot.py
+++ robot_localization/robot.py
@@ -14,8 +14,18 @@ class Robot:
             "pHit": sensor_accuracy_constants["pHit"],
             "pMiss": sensor_accuracy_constants["pMiss"]
         }
+    
+    def sense_and_move(self, grid, measurements, movements):
+        for index, measurement in enumerate(measurements):
+            self.sense(grid, measurements[index])
+            self.move(grid, movements[index])
+        
+        return grid.get_state("probability")
             
     def sense(self, grid, sensor_readings):
+        if isinstance(sensor_readings, str):
+            sensor_readings = [sensor_readings]
+        
         for reading in sensor_readings:
             grid.evaluate_posterior_probabilities(reading, self.sensor_accuracy_constants)
             grid.normalize_probabilities()
@@ -44,7 +54,6 @@ class Robot:
             compiled_distribution = [x+y for x,y in zip(compiled_distribution, specific_cell_distribution)]
         
         grid.set_state("probability", compiled_distribution)
-        
         return grid.get_state("probability")
         
         
